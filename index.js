cc89df245f53cdddc9f07ce76739adc27fb13bd3
stepanzak <stepka279.zak@gmail.com>
2023-02-14 22:02:38 +0100

    installed MongoDB and added it to index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+//mongoose for interacting with mongodb
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/projectman', { useNewUrlParser: true, family: 4 })
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch(error => console.error('Could not connect to MongoDB... ', error));
+
 
 
 app.get('/', (req, res) => {
