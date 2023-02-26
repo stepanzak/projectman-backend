0afe1396d3614ae2547dcab1c5daf0fd79210a81
stepanzak <stepka279.zak@gmail.com>
2023-02-26 18:32:51 +0100

    added get and post thingies for all the categories

--- index.js
+++ index.js
@@ -10,9 +10,141 @@ mongoose.connect('mongodb://localhost:27017/projectman', { useNewUrlParser: true
   .catch(error => console.error('Could not connect to MongoDB... ', error));
 
 
+const expenseSchema = new mongoose.Schema({
+  name: String,
+  task: String,
+  cost: Number,
+  date: Date,
+  description: String
+})
+
+const workSchema = new mongoose.Schema({
+  dateStart: Date,
+  dateEnd: Date,
+  worker: String,
+  task: String,
+  isFixedPrize: Boolean, //if not, it's hourly rate
+  cost: Number,
+  description: String
+})
+
+const customerSchema = new mongoose.Schema({
+  name: String,
+})
+
+const projectSchema = new mongoose.Schema({
+  name: String,
+  customer: String,
+  description: String,
+  isDone: Boolean
+})
+
+const taskSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  project: String,
+  isDone: Boolean
+})
+
+const workerSchema = new mongoose.Schema({
+  name: String
+})
+
+
+const Expense = mongoose.model('Expense', expenseSchema);
+const Work = mongoose.model('Work', workSchema);
+const Customer = mongoose.model('Customer', customerSchema);
+const Project = mongoose.model('Project', projectSchema);
+const Task = mongoose.model('Task', taskSchema);
+const Worker = mongoose.model('Worker', workerSchema);
+
+
 
 app.get('/', (req, res) => {
     res.send("jarel je karel")
+})
+
+app.post('/expenses', (req, res) => {
+  const expense = new Expense(req.body)
+  console.log(expense)
+  expense.save()
+  res.send('ok')
+})
+
+app.post('/work', (req, res) => {
+  const work = new Work(req.body)
+  console.log(work)
+  work.save()
+  res.send('ok')
+})
+
+app.post('/customer', (req, res) => {
+  const customer = new Customer(req.body)
+  console.log(customer)
+  customer.save()
+  res.send('ok')
+})
+
+app.post('/project', (req, res) => {
+  const project = new Project(req.body)
+  console.log(project)
+  project.save()
+  res.send('ok')
+})
+
+app.post('/task', (req, res) => {
+  const task = new Task(req.body)
+  console.log(task)
+  task.save()
+  res.send('ok')
+})
+
+app.post('/worker', (req, res) => {
+  const worker = new Worker(req.body)
+  console.log(worker)
+  worker.save()
+  res.send('ok')
+})
+
+
+// GET requests
+
+
+app.get('/expenses', (req, res) => {
+  Expense.find({}, (err, expenses) => {
+    res.send(expenses)
+  })
+})
+
+
+app.get('/work', (req, res) => {
+  Work.find({}, (err, works) => {
+    res.send(works)
+  })
+})
+
+app.get('/customer', (req, res) => {
+  Customer.find({}, (err, customers) => {
+    res.send(customers)
+  })
+})
+
+app.get('/project', (req, res) => {
+  Project.find({}, (err, projects) => {
+    res.send(projects)
+  })
+})
+
+app.get('/task', (req, res) => {
+  Task.find({}, (err, tasks) => {
+    res.send(tasks)
+  })
+})
+
+app.get('/worker', (req, res) => {
+  Worker.find({}, (err, workers) => {
+    res.send(workers)
+  })
 })
 
 
